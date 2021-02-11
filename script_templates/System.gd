57bc74cb84ed5a5cc45514e712e1c56e40421542
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-11 16:06:34 +0100

    Fix System template

--- script_templates/System.gd
+++ script_templates/System.gd
@@ -1,8 +1,5 @@
 class_name __System
 extends System
 
-# func on_process_entity(entity: Entity, delta: float):
-# 	pass
-
-# func on_process_entities(entities: Array, delta: float):
-# 	pass
+func on_process_entity(entity: Entity, delta: float):
+	pass
