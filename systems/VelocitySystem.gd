6522393623eb9528cee42f7e56f783ad35ed9c7f
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-11 16:06:43 +0100

    Clean up comment

--- systems/VelocitySystem.gd
+++ systems/VelocitySystem.gd
@@ -5,6 +5,3 @@ extends System
 func on_process_entity(entity: Entity, delta: float):
 	var _component = entity.get_component("velocity") as Velocity
 	entity.position += _component.direction * _component.speed * delta
-
-# func on_process(entities: Array, delta: float):
-# 	pass
