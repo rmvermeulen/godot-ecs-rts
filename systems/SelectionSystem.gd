1fd0ae25c9588b8b24edb70f5e733bc1c148ec10
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-13 20:42:20 +0100

    Make selection system simpler

--- systems/SelectionSystem.gd
+++ systems/SelectionSystem.gd
@@ -1,11 +1,9 @@
 class_name SelectionSystem
 extends System
 
-enum Action { SELECT_SINGLE, DESELECT_ALL, CHECK_RECT }
-
-var selection_event_data = null
-var next_entity_action = null
-var single_selected_entity = null
+# any selection event has some data (position, rect, ...)
+var _selection_event_data = null
+var _selected_entities = []
 
 
 func _ready():
@@ -15,64 +13,67 @@ func _ready():
 
 func _on_selected_screen_pos(screen_pos: Vector2):
 	# TODO: figure out actual world position
-	selection_event_data = screen_pos
+	_selection_event_data = screen_pos
 
 
 func _on_selected_screen_rect(screen_rect: Rect2):
 	# TODO: figure out actual world position
-	selection_event_data = screen_rect
+	_selection_event_data = screen_rect
 
 
-func on_process(entities: Array, delta: float):
-	if selection_event_data == null:
+func on_process(entities: Array, _delta: float):
+	# only run if a selection event occorred
+	if _selection_event_data == null:
 		return
 
-	Logger.debug('[SelectionSystem] processing entities for event_data: %s' % selection_event_data)
+	Logger.debug('[SelectionSystem] processing entities for event_data: %s' % _selection_event_data)
 
-	match typeof(selection_event_data):
+	# check what kind of selection is happening
+	match typeof(_selection_event_data):
 		TYPE_VECTOR2:
+			# select the first entity on this position
+
 			var results: Array = get_parent().get_world_2d().direct_space_state.intersect_point(
-				selection_event_data, 1
+				_selection_event_data, 1
 			)
-			if results.empty():
-				next_entity_action = Action.DESELECT_ALL
-			else:
-				var collider = results[0].collider
-				if entities.has(collider):
-					next_entity_action = Action.SELECT_SINGLE
-					single_selected_entity = collider
-				else:
-					next_entity_action = Action.DESELECT_ALL
-
+			var collider = null if results.empty() else results[0].collider
+			select_single(entities, collider)
 		TYPE_RECT2:
-			next_entity_action = Action.CHECK_RECT
+			select_rect(entities, _selection_event_data)
 
+	# clear event data
+	_selection_event_data = null
+
+
+func deselect_all(entities: Array):
+	_selected_entities = []
 	for entity in entities:
-		on_process_entity(entity, delta)
-
-	selection_event_data = null
-	next_entity_action = null
-	single_selected_entity = null
+		var component: Selection = entity.get_component("selection")
+		component.selected = false
 
 
-func on_process_entity(entity: Entity, _delta: float):
-	Logger.trace("[SelectionSystem]: processing entity: %s" % entity)
-	var c := entity.get_component('selection') as Selection
-
-	match next_entity_action:
-		Action.DESELECT_ALL:
-			c.selected = false
-		Action.SELECT_SINGLE:
-			c.selected = entity == single_selected_entity
-		Action.CHECK_RECT:
-			c.selected = is_inside_rect(entity)
-
-	if c.selected:
-		Logger.debug('[SelectionSystem] entity selected: %s' % entity)
+func select_single(entities: Array, selected_entity: Entity):
+	_selected_entities = [selected_entity]
+	for entity in entities:
+		var component: Selection = entity.get_component("selection")
+		component.selected = (entity == selected_entity)
 
 
-func is_inside_rect(entity: Entity) -> bool:
-	assert(selection_event_data is Rect2)
-	var urect := Rect2(-32, -32, 64, 64)
-	urect.position += entity.position
-	return urect.intersects(selection_event_data)
+func select_rect(entities: Array, rect: Rect2):
+	_selected_entities = []
+	for entity in entities:
+		var component: Selection = entity.get_component("selection")
+
+		var entity_rect := Rect2(-32, -32, 64, 64)
+		entity_rect.position += entity.position
+		if entity_rect.intersects(rect):
+			component.selected = true
+			_selected_entities.append(entity)
+
+
+func get_selected_entities() -> Array:
+	return _selected_entities
+
+
+func on_process_entity(_entity: Entity, _delta: float):
+	assert(false, "Unused method")
