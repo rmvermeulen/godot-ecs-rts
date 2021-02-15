705b205b3195a35ec99bb99dd8906855d7ce8cd1
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-15 22:41:25 +0100

    QuadTreeTest: add more get_rect() cases

--- tests/QuadTree.test.gd
+++ tests/QuadTree.test.gd
@@ -68,4 +68,10 @@ func test_get_rect():
 	assert_eq(quad.get_rect(3), Rect2(0, 64, 64, 64))
 	assert_eq(quad.get_rect(4), Rect2(64, 64, 64, 64))
 
-	assert_eq(quad.get_rect(5), Rect2(64, 64, 25, 25))
+	assert_eq(quad.get_rect(5), Rect2(0, 0, 32, 32))
+	assert_eq(quad.get_rect(8), Rect2(32, 32, 32, 32))
+
+	assert_eq(quad.get_rect(15e1), Rect2(72, 0, 8, 8))
+	assert_eq(quad.get_rect(15e2), Rect2(6, 18, 2, 2))
+	assert_eq(quad.get_rect(10e3), Rect2(85, 15, 1, 1))
+	assert_eq(quad.get_rect(10e5), Rect2(53.125, 93.875, 0.125, 0.125))
