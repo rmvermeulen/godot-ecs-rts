6cef47277bdb8a4772361cb7c8ddd7aebae68388
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-17 23:27:56 +0100

    Unit entity: pass on team.color to sprite

--- entities/Unit.gd
+++ entities/Unit.gd
@@ -2,7 +2,11 @@ class_name Unit
 extends Entity
 
 # func on_init():pass
-# func on_ready():pass
+
+
+func on_ready():
+	$Sprite.modulate = get_component('team').color
+
 # func on_enter_tree():pass
 # func on_exit_tree():pass
 # func on_before_add():pass
