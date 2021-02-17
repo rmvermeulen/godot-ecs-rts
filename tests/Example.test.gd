24a9b3aeba9a545b8dfaaeb96ea175c1171c7bdc
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-17 21:46:32 +0100

    Add example test to ensure the runner works

---
+++ tests/Example.test.gd
@@ -0,0 +1,21 @@
+extends "res://addons/gut/test.gd"
+
+
+func before_all():
+	pass
+
+
+func before_each():
+	pass
+
+
+func after_each():
+	pass
+
+
+func after_all():
+	pass
+
+
+func test_example_case():
+	assert_eq(3 + 5, 8, "Math works!")
