390c83b8dc07d65e90f406a941542310ef9ce4a9
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-17 23:48:18 +0100

    Add some structures to the map

---
+++ entities/Structure.gd
@@ -0,0 +1,13 @@
+class_name Structure
+extends Entity
+
+
+# func on_init():pass
+func on_ready():
+	$Sprite.modulate = get_component('team').color
+# func on_enter_tree():pass
+# func on_exit_tree():pass
+# func on_before_add():pass
+# func on_after_add():pass
+# func on_before_remove():pass
+# func on_after_remove():pass
