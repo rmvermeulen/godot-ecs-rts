4afe9dd85e6db35a222753d2c7915b1ab2a49a0e
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2021-02-17 22:56:12 +0100

    Overlay: use system cursor custom image instead of _draw()

--- src/Overlay.gd
+++ src/Overlay.gd
@@ -1,15 +1,15 @@
 extends Node2D
 
-export (int, 1, 200) var cursor_type := 39 setget set_type
-export (Color) var cursor_color := Color.white
-var cursor_texture: Texture = null
+export (int, 1, 200) var cursor_type := 39 setget set_cursor_type
+export (Color) var cursor_color := Color.white setget set_cursor_color
 
 var drag_screen_rect = null
 
 
 func _ready() -> void:
 	assert(OK == Events.connect("pre_select_screen_rect", self, "_on_pre_select_screen_rect"))
-	set_type(cursor_type)
+	set_cursor_type(cursor_type)
+	# Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 func _process(_delta: float) -> void:
@@ -26,17 +26,20 @@ func _input(event: InputEvent) -> void:
 				self.cursor_type += 1
 
 
-func set_type(value: int) -> void:
-	if value >= 1 && value <= 200:
-		cursor_type = value
-		cursor_texture = load("res://assets/crosshair-pack/crosshair%03d.png" % cursor_type)
+func set_cursor_type(value: int) -> void:
+	if value < 1 && value > 200:
+		return
+	cursor_type = value
+	var tex: Texture = load("res://assets/crosshair-pack/crosshair%03d.png" % cursor_type)
+	Input.set_custom_mouse_cursor(tex, 0, Vector2(32, 32))
+
+
+func set_cursor_color(value: Color) -> void:
+	cursor_color = value
+	update()
 
 
 func _draw() -> void:
-	# draw cursor
-	var mp := get_local_mouse_position()
-	if cursor_texture:
-		draw_texture(cursor_texture, mp - Vector2(32, 32), cursor_color)
 	# draw selection box
 	if drag_screen_rect:
 		draw_rect(drag_screen_rect, Color.green, false)
